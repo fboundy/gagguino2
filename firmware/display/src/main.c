8448c4dd10f8cb876edf0b3f034e5aa040649c26
fboundy <foboundy@gmail.com>
2025-09-08 22:34:26 +0100

    Slow LVGL update loop to 250ms

--- firmware/display/src/main.c
+++ firmware/display/src/main.c
@@ -61,7 +61,8 @@ void app_main(void)
 
     while (1) {
         // raise the task priority of LVGL and/or reduce the handler period can improve the performance
-        vTaskDelay(pdMS_TO_TICKS(10));
+        // run lv_timer_handler every 250 ms
+        vTaskDelay(pdMS_TO_TICKS(250));
         // The task running lv_timer_handler should have lower priority than that running `lv_tick_inc`
         lv_timer_handler();
     }
