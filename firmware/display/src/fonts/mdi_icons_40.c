1a8cafa3b5cca40df5a3d166be2a1ad0b426a4b1
fboundy <foboundy@gmail.com>
2025-09-09 20:58:39 +0100

    Font fix

--- firmware/display/src/fonts/mdi_icons_40.c
+++ firmware/display/src/fonts/mdi_icons_40.c
@@ -1,24 +1,22 @@
 /*******************************************************************************
  * Size: 40 px
  * Bpp: 1
- * Opts: --bpp 1 --size 40 --no-compress --stride 1 --align 1 --font materialdesignicons-webfont.ttf --range 984335,983706,984347,984211,989081,986346,987295 --format lvgl -o mdi_icons_40.c
+ * Opts: --bpp 1 --size 40 --no-compress --stride 1 --align 1 --font materialdesignicons-webfont.ttf --range 984335,983706,984347,984211,989081,986346,987295,984101 --format lvgl -o mdi_icons_40.c
  ******************************************************************************/
 
 #ifdef __has_include
-    #if __has_include("lvgl.h")
-        #ifndef LV_LVGL_H_INCLUDE_SIMPLE
-            #define LV_LVGL_H_INCLUDE_SIMPLE
-        #endif
-    #endif
+#if __has_include("lvgl.h")
+#ifndef LV_LVGL_H_INCLUDE_SIMPLE
+#define LV_LVGL_H_INCLUDE_SIMPLE
+#endif
+#endif
 #endif
 
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
-    #include "lvgl.h"
+#include "lvgl.h"
 #else
-    #include "lvgl/lvgl.h"
+#include "lvgl/lvgl.h"
 #endif
-
-
 
 #ifndef MDI_ICONS_40
 #define MDI_ICONS_40 1
@@ -51,6 +49,20 @@ static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {
     0x1f, 0xff, 0xff, 0x0, 0x3, 0xff, 0xfe, 0x0,
     0x0, 0x7f, 0xfc, 0x0, 0x0, 0x7, 0xf8, 0x0,
     0x0,
+
+    /* U+F0425 "󰐥" */
+    0x0, 0xe, 0x0, 0x0, 0x1, 0xc0, 0x0, 0x0,
+    0x38, 0x0, 0x0, 0x7, 0x0, 0x0, 0x40, 0xe0,
+    0x40, 0x1c, 0x1c, 0x1c, 0x7, 0xc3, 0x87, 0xc1,
+    0xe0, 0x70, 0x3c, 0x38, 0xe, 0x3, 0x8f, 0x1,
+    0xc0, 0x79, 0xc0, 0x38, 0x7, 0x78, 0x7, 0x0,
+    0xfe, 0x0, 0xe0, 0xf, 0xc0, 0x1c, 0x1, 0xf8,
+    0x3, 0x80, 0x3f, 0x0, 0x70, 0x7, 0xe0, 0x0,
+    0x0, 0xfc, 0x0, 0x0, 0x1d, 0xc0, 0x0, 0x7,
+    0x38, 0x0, 0x0, 0xe7, 0x80, 0x0, 0x3c, 0x78,
+    0x0, 0xf, 0x7, 0x80, 0x3, 0xc0, 0xf8, 0x0,
+    0xf8, 0xf, 0xc0, 0x7e, 0x0, 0x7f, 0xff, 0x0,
+    0x7, 0xff, 0xc0, 0x0, 0x1f, 0xc0, 0x0,
 
     /* U+F0493 "󰒓" */
     0x0, 0x7, 0xf0, 0x0, 0x0, 0x7, 0xfc, 0x0,
@@ -153,9 +165,7 @@ static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {
     0x0, 0x3, 0xce, 0x0, 0x0, 0x0, 0xe7, 0x0,
     0x0, 0x0, 0x73, 0x80, 0x0, 0x0, 0x39, 0xc0,
     0x0, 0x0, 0x1c, 0xe0, 0x0, 0x0, 0xe, 0x70,
-    0x0, 0x0, 0x7, 0x38
-};
-
+    0x0, 0x0, 0x7, 0x38};
 
 /*---------------------
  *  GLYPH DESCRIPTION
@@ -164,32 +174,25 @@ static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {
 static const lv_font_fmt_txt_glyph_dsc_t glyph_dsc[] = {
     {.bitmap_index = 0, .adv_w = 0, .box_w = 0, .box_h = 0, .ofs_x = 0, .ofs_y = 0} /* id = 0 reserved */,
     {.bitmap_index = 0, .adv_w = 640, .box_w = 33, .box_h = 33, .ofs_x = 3, .ofs_y = -2},
-    {.bitmap_index = 137, .adv_w = 640, .box_w = 33, .box_h = 34, .ofs_x = 3, .ofs_y = -2},
-    {.bitmap_index = 278, .adv_w = 640, .box_w = 18, .box_h = 33, .ofs_x = 11, .ofs_y = -2},
-    {.bitmap_index = 353, .adv_w = 640, .box_w = 30, .box_h = 35, .ofs_x = 5, .ofs_y = -2},
-    {.bitmap_index = 485, .adv_w = 640, .box_w = 30, .box_h = 30, .ofs_x = 5, .ofs_y = 0},
-    {.bitmap_index = 598, .adv_w = 640, .box_w = 26, .box_h = 33, .ofs_x = 7, .ofs_y = -2},
-    {.bitmap_index = 706, .adv_w = 640, .box_w = 33, .box_h = 32, .ofs_x = 3, .ofs_y = -1}
-};
+    {.bitmap_index = 137, .adv_w = 640, .box_w = 27, .box_h = 28, .ofs_x = 7, .ofs_y = 2},
+    {.bitmap_index = 232, .adv_w = 640, .box_w = 33, .box_h = 34, .ofs_x = 3, .ofs_y = -2},
+    {.bitmap_index = 373, .adv_w = 640, .box_w = 18, .box_h = 33, .ofs_x = 11, .ofs_y = -2},
+    {.bitmap_index = 448, .adv_w = 640, .box_w = 30, .box_h = 35, .ofs_x = 5, .ofs_y = -2},
+    {.bitmap_index = 580, .adv_w = 640, .box_w = 30, .box_h = 30, .ofs_x = 5, .ofs_y = 0},
+    {.bitmap_index = 693, .adv_w = 640, .box_w = 26, .box_h = 33, .ofs_x = 7, .ofs_y = -2},
+    {.bitmap_index = 801, .adv_w = 640, .box_w = 33, .box_h = 32, .ofs_x = 3, .ofs_y = -1}};
 
 /*---------------------
  *  CHARACTER MAPPING
  *--------------------*/
 
 static const uint16_t unicode_list_0[] = {
-    0x0, 0x1f9, 0x275, 0x281, 0xa50, 0xe05, 0x14ff
-};
+    0x0, 0x18b, 0x1f9, 0x275, 0x281, 0xa50, 0xe05, 0x14ff};
 
 /*Collect the unicode lists and glyph_id offsets*/
 static const lv_font_fmt_txt_cmap_t cmaps[] =
-{
     {
-        .range_start = 983706, .range_length = 5376, .glyph_id_start = 1,
-        .unicode_list = unicode_list_0, .glyph_id_ofs_list = NULL, .list_length = 7, .type = LV_FONT_FMT_TXT_CMAP_SPARSE_TINY
-    }
-};
-
-
+        {.range_start = 983706, .range_length = 5376, .glyph_id_start = 1, .unicode_list = unicode_list_0, .glyph_id_ofs_list = NULL, .list_length = 8, .type = LV_FONT_FMT_TXT_CMAP_SPARSE_TINY}};
 
 /*--------------------
  *  ALL CUSTOM DATA
@@ -197,7 +200,7 @@ static const lv_font_fmt_txt_cmap_t cmaps[] =
 
 #if LVGL_VERSION_MAJOR == 8
 /*Store all the custom data of the font*/
-static  lv_font_fmt_txt_glyph_cache_t cache;
+static lv_font_fmt_txt_glyph_cache_t cache;
 #endif
 
 #if LVGL_VERSION_MAJOR >= 8
@@ -220,8 +223,6 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
 
 };
 
-
-
 /*-----------------
  *  PUBLIC FONT
  *----------------*/
@@ -232,10 +233,10 @@ const lv_font_t mdi_icons_40 = {
 #else
 lv_font_t mdi_icons_40 = {
 #endif
-    .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
-    .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/
-    .line_height = 35,          /*The maximum line height required by the font*/
-    .base_line = 2,             /*Baseline measured from the bottom of the line*/
+    .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt, /*Function pointer to get glyph's data*/
+    .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt, /*Function pointer to get glyph's bitmap*/
+    .line_height = 35,                              /*The maximum line height required by the font*/
+    .base_line = 2,                                 /*Baseline measured from the bottom of the line*/
 #if !(LVGL_VERSION_MAJOR == 6 && LVGL_VERSION_MINOR == 0)
     .subpx = LV_FONT_SUBPX_NONE,
 #endif
@@ -243,14 +244,11 @@ lv_font_t mdi_icons_40 = {
     .underline_position = 0,
     .underline_thickness = 0,
 #endif
-    .static_bitmap = 0,
-    .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
+    .dsc = &font_dsc, /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 #if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
     .fallback = NULL,
 #endif
     .user_data = NULL,
 };
-
-
 
 #endif /*#if MDI_ICONS_40*/
